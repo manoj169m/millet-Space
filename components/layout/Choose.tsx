7a195c91fa8fe2d82074cc40a633701e5c5470e3
manoj169m <manoj169@gmail.com>
2025-04-05 21:55:34 +0530

    left

--- components/layout/Choose.tsx
+++ components/layout/Choose.tsx
@@ -16,9 +16,9 @@ const Choose = () => {
         </div>
 
         {/* Right side (Text) */}
-        <div className="flex-1 text-center md:text-left">
+        <div className="flex-1 text-left md:text-left">
           <h2 className="text-3xl font-bold text-green-800 mb-4">Why Choose Millet Space?</h2>
-          <ul className="list-disc list-inside text-lg text-gray-700 space-y-4">
+          <ul className="list-none list-inside text-lg text-gray-700 space-y-4">
             <li><span className="font-semibold text-green-800">100% Organic:</span> All our products are certified organic, free from pesticides and harmful chemicals.</li>
             <li><span className="font-semibold text-green-800">Sustainable and Eco-Friendly:</span> We care about the planet, and so should you. Our products are sourced sustainably and packaged with care.</li>
             <li><span className="font-semibold text-green-800 ">Fresh and Natural:</span> Delivered straight to your door, our organic items retain their natural freshness and nutritional value.</li>
